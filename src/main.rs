54943009f5f5fc91e02933f7e9ced1a66db49c6c
Ryo Arima <ryo.arima@outlook.com>
2024-08-07 02:38:36 +0900

    tmp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 10;
+    let mut y = 20;
+
+    let mut r: *mut i32 = &mut x;
+    println!("1 x: {}", unsafe { *r });
+
+    let rr: *mut *mut i32 = &mut r;
+
+    unsafe {
+        **rr += 1;
+        println!("2 x: {}", x);
+        println!("3 x: {}", *r);
+        
+        *rr = &mut y;
+        println!("4 y: {}", *r);
+    }
 }
